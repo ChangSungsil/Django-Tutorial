bfa58d1c474cd00fdc5aadf0f53028218d693ca3
Sungsil Chang <sungsil.universe@gmail.com>
2017-11-13 20:27:12 +0900

    change database > mysql

--- stdv1/settings.py
+++ stdv1/settings.py
@@ -76,8 +76,14 @@ WSGI_APPLICATION = 'stdv1.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        #'ENGINE': 'django.db.backends.sqlite3',
+        #'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django',
+        'USER': 'root',
+        'PASSWORD': 's1luniv@670',
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
